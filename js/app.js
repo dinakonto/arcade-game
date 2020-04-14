30f5ec084f1be8dc0dc0dced5301284c4ccaf87d
Dina Konto <dina.konto@gmail.com>
2020-04-14 13:24:15 +1000

    Update syntax to use Classes. Player+enemies shown

--- js/app.js
+++ js/app.js
@@ -1,39 +1,60 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+class Enemy {
+  constructor() { // Variables applied to each enemy
     this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+    this.x = -120; // Position off-screen to the left - make random
+    this.yArr = [55, 135, 220]; // One for each row
+    this.y = 55; // Choose one from the array at random
+    this.speed = 50; // Variable speed, at random
+  }
+  update(dt) { // Move the enemy along the x axis
+    this.x += (this.speed * dt); // Multiply by dt to run same speed on all computers
+  }
+  render() { // Draw the enemy on screen
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
+class Player {
+  constructor() {
+    this.x = 201;
+    this.y = 380;
+    this.sprite = 'images/char-boy.png';
+  }
+  update() {
 
+  }
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  handleInput(k) {
+    const move = 40;
+    switch (k) {
+      case 'left':
+        console.log('Pressed left');
+        this.x -= move;
+        break;
+      case 'up':
+        console.log('Pressed up');
+        this.y -= move;
+        break;
+      case 'right':
+        console.log('Pressed right');
+        this.x += move;
+        break;
+      case 'down':
+        console.log('Pressed down');
+        this.y += move;
+        break;
+    }
+  }
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate the player and 3 enemies
+const allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+const player = new Player();
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for key presses and send the keys to the Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
