404d5a146da20231a7c32cce9dd1e9c1ffedd9d5
Dina Konto <dina.konto@gmail.com>
2020-04-14 14:21:57 +1000

    Added boundaries to player movement

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// BUGS TO FIX
+// - enemy speed is often slower than the specified min
+
 // Random number function
 function randomNum(min, max) {
   return Math.random() * (max - min + 1);
@@ -6,7 +9,7 @@ function randomNum(min, max) {
 class Enemy {
   constructor() { // Variables applied to each instance
     this.sprite = 'images/enemy-bug.png';
-    this.x = (-80 - randomNum(10, 50)); // Position off-screen to the left - make random
+    this.x = (-100 - randomNum(10, 50)); // Position off-screen to the left - make random
     this.yArr = [55, 135, 220]; // One for each row
     this.y = this.yArr[Math.floor(randomNum(1, 3))]; // Choose one from the array at random
     this.speed = randomNum(100, 400); // Variable speed, at random
@@ -14,7 +17,7 @@ class Enemy {
   update(dt) { // Move the enemy along the x axis
     this.x += (this.speed * dt); // Multiply by dt to run same speed on all computers
     if (this.x > 505) { // Once it reaches the end, reset it
-      this.x = -50;
+      this.x = -100;
       this.speed = randomNum(100, 400);
       this.y = this.yArr[Math.floor(randomNum(1, 3))];
     }
@@ -32,25 +35,41 @@ class Player {
     this.sprite = 'images/char-boy.png';
   }
   update() {
-
+    
   }
   render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
   handleInput(k) { // k = key pressed
-    const m = 40; // m = the distance each keypress moves
     switch (k) {
       case 'left':
-        this.x -= m;
-        break;
+        if (this.x > 20) {
+          this.x -= 101;
+          break;
+        } else {
+          break;
+        }
       case 'up':
-        this.y -= m;
-        break;
+        if (this.y > 0) {
+          this.y -= 82;
+          break;
+        } else {
+          break;
+        }
       case 'right':
-        this.x += m;
-        break;
+        if (this.x < 401) {
+          this.x += 101;
+          break;
+        } else {
+          break;
+      }
       case 'down':
-        this.y += m;
+        if (this.y < 380) {
+          this.y += 82;
+          break;
+        } else {
+          break;
+        }
     }
   }
 };
